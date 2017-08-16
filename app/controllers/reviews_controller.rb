277f32284f04abd13bae5ffbb81d29a4759f3cc4
郭春日 <uochunri@gmail.com>
2017-08-17 00:32:43 +0800

    Convert review ratings to starts

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,24 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
-
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1.json
-  def show
-  end
 
   def new
     @review = Review.new
   end
 
-  # GET /reviews/1/edit
   def edit
   end
 
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     if @review.save
-      redirect_to @review
+      redirect_to @movie
     else
-      render :new
+      render 'new'
     end
   end
 
@@ -39,12 +32,14 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+
     def review_params
       params.require(:review).permit(:rating, :comment)
     end
